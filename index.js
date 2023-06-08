5f4c017073e7ed7e49c89674c5e95f195a3e13e2
pitamchandra <pitamchandra402@gmail.com>
2023-06-09 01:33:04 +0600

    dotenv install and require it done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) =>{
     res.send('school server is running')
